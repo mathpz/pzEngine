208e36ff3a7a2b7cb86e3fb9f44549195ab09bab
mathpz <matheus.pedrozo@gmail.com>
2023-08-04 15:00:38 -0300

    added pushConstants

--- src/first_app.cpp
+++ src/first_app.cpp
@@ -1,5 +1,10 @@
 #include "first_app.hpp"
 
+// libs
+#define  GLM_FORCE_RADIANS
+#define  GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <glm/glm.hpp>
+
 // std
 #include <array>
 #include <cassert>
@@ -7,6 +12,12 @@
 
 namespace pz
 {
+
+    struct SimplePushConstantsData
+    {
+        glm::vec2 offset;
+        alignas(16) glm::vec3 color;
+    };
 
     FirstApp::FirstApp()
     {
@@ -40,12 +51,17 @@ namespace pz
 
     void FirstApp::createPipelineLayout()
     {
+        VkPushConstantRange pushConstantRange{};
+        pushConstantRange.stageFlags = VK_SHADER_STAGE_VERTEX_BIT | VK_SHADER_STAGE_FRAGMENT_BIT;
+        pushConstantRange.offset = 0;
+        pushConstantRange.size = sizeof(SimplePushConstantsData);
+
         VkPipelineLayoutCreateInfo pipelineLayoutInfo{};
         pipelineLayoutInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO;
         pipelineLayoutInfo.setLayoutCount = 0;
         pipelineLayoutInfo.pSetLayouts = nullptr;
-        pipelineLayoutInfo.pushConstantRangeCount = 0;
-        pipelineLayoutInfo.pPushConstantRanges = nullptr;
+        pipelineLayoutInfo.pushConstantRangeCount = 1;
+        pipelineLayoutInfo.pPushConstantRanges = &pushConstantRange;
         if (vkCreatePipelineLayout(pzDevice.device(), &pipelineLayoutInfo, nullptr, &pipelineLayout) !=
             VK_SUCCESS)
         {
@@ -117,6 +133,9 @@ namespace pz
 
     void FirstApp::recordCommandBuffer(int imageIndex)
     {
+        static int frame = 0;
+        frame = (frame + 1) % 100;
+
         VkCommandBufferBeginInfo beginInfo{};
         beginInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
 
@@ -134,7 +153,7 @@ namespace pz
         renderPassInfo.renderArea.extent = pzSwapChain->getSwapChainExtent();
 
         std::array<VkClearValue, 2> clearValues{};
-        clearValues[0].color = {0.1f, 0.1f, 0.1f, 1.0f};
+        clearValues[0].color = {0.01f, 0.01f, 0.01f, 1.0f};
         clearValues[1].depthStencil = {1.0f, 0};
         renderPassInfo.clearValueCount = static_cast<uint32_t>(clearValues.size());
         renderPassInfo.pClearValues = clearValues.data();
@@ -154,7 +173,17 @@ namespace pz
 
         pzPipeline->bind(commandBuffers[imageIndex]);
         pzModel->bind(commandBuffers[imageIndex]);
-        pzModel->draw(commandBuffers[imageIndex]);
+
+        for(int j = 0; j < 4; j++)
+        {
+            SimplePushConstantsData push{};
+            push.offset = {-0.5f + frame * 0.02f, -0.4f + j * 0.25f};
+            push.color = {0.0f, 0.0f, 0.2f + j * 0.2f};
+
+            vkCmdPushConstants(commandBuffers[imageIndex], pipelineLayout, VK_SHADER_STAGE_VERTEX_BIT | VK_SHADER_STAGE_FRAGMENT_BIT, 0, sizeof(SimplePushConstantsData), &push);
+            pzModel->draw(commandBuffers[imageIndex]);
+        }
+
 
         vkCmdEndRenderPass(commandBuffers[imageIndex]);
         if (vkEndCommandBuffer(commandBuffers[imageIndex]) != VK_SUCCESS)
